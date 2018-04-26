dd8a3667f7e258ae1d90aa80b59cb95e00fbcb98
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2018-04-26 15:59:44 +0200

    test for 2.0

--- script.js
+++ script.js
@@ -14,21 +14,22 @@ function loadDoc() {
 
             for (var i = 0; i < 11; i++) {
                 "srcinstagram" + i = objeto.graphql.hashtag.edge_hashtag_to_media.edges[+'"' + i '"' + ].node.display_url;
+                document.getElementById("foto" + i).src = srcinstagram + i;
+
             }
 
             /*
-                        srcinstagram1 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["0"].node.display_url;
-                        srcinstagram2 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["1"].node.display_url;
-                        srcinstagram3 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["2"].node.display_url;
-                        srcinstagram4 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["3"].node.display_url;
-                        srcinstagram5 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["4"].node.display_url;
-                        srcinstagram6 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["5"].node.display_url;
-                        srcinstagram7 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["6"].node.display_url;
-                        srcinstagram8 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["7"].node.display_url;
-                        srcinstagram9 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["8"].node.display_url;
-                        srcinstagram10 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["9"].node.display_url;
-                        srcinstagram11 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["10"].node.display_url;
-            */
+            srcinstagram1 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["0"].node.display_url;
+            srcinstagram2 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["1"].node.display_url;
+            srcinstagram3 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["2"].node.display_url;
+            srcinstagram4 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["3"].node.display_url;
+            srcinstagram5 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["4"].node.display_url;
+            srcinstagram6 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["5"].node.display_url;
+            srcinstagram7 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["6"].node.display_url;
+            srcinstagram8 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["7"].node.display_url;
+            srcinstagram9 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["8"].node.display_url;
+            srcinstagram10 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["9"].node.display_url;
+            srcinstagram11 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["10"].node.display_url;
             document.getElementById("foto1").src = srcinstagram1;
             document.getElementById("foto2").src = srcinstagram2;
             document.getElementById("foto3").src = srcinstagram3;
@@ -40,6 +41,8 @@ function loadDoc() {
             document.getElementById("foto9").src = srcinstagram9;
             document.getElementById("foto10").src = srcinstagram10;
             document.getElementById("foto11").src = srcinstagram11;
+            */
+
         }
     };
     xhttp.open("GET", urlapi, true);
