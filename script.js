1c54330e43ed37a8ab2839641fd0d47767f98188
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2018-04-26 15:47:03 +0200

    Auto stash before rebase of "origin/master"

--- script.js
+++ script.js
@@ -2,18 +2,14 @@ function loadDoc() {
     var d = new Date();
     var xhttp;
     var urlapi;
-    urlapi =  "https://www.instagram.com/explore/tags/"+ document.getElementById("inputtext").value + "/?__a=1"
+    urlapi = "https://www.instagram.com/explore/tags/" + document.getElementById("inputtext").value + "/?__a=1"
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-        
-
         if (this.readyState == 4 && this.status == 200) {
             var objeto = JSON.parse(xhttp.responseText);
-
-            var srcinstagram1,srcinstagram2,srcinstagram3,srcinstagram4,srcinstagram5,srcinstagram6,srcinstagram7,
-                srcinstagram8,srcinstagram9,srcinstagram10,srcinstagram11;
-            
-            
+            var srcinstagram1, srcinstagram2, srcinstagram3, srcinstagram4, srcinstagram5, srcinstagram6, srcinstagram7,
+                srcinstagram8, srcinstagram9, srcinstagram10, srcinstagram11;
+            alert(objeto.graphql.hashtag.edge_hashtag_to_media.edges.length);
             srcinstagram1 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["0"].node.display_url;
             srcinstagram2 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["1"].node.display_url;
             srcinstagram3 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["2"].node.display_url;
@@ -25,8 +21,6 @@ function loadDoc() {
             srcinstagram9 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["8"].node.display_url;
             srcinstagram10 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["9"].node.display_url;
             srcinstagram11 = objeto.graphql.hashtag.edge_hashtag_to_media.edges["10"].node.display_url;
-            
-
             document.getElementById("foto1").src = srcinstagram1;
             document.getElementById("foto2").src = srcinstagram2;
             document.getElementById("foto3").src = srcinstagram3;
